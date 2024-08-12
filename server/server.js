f7331ff56ca6314dc2a7a51521c1f87e0e9fa413
Macarena Riquelme <meriquelmece@gmail.com>
2024-08-11 22:19:50 -0400

    Actualización de URL de solicitudes

--- server/server.js
+++ server/server.js
@@ -13,22 +13,21 @@ config();
 const app = express();
 const port = process.env.PORT || 8000;
 
-// Middlewares de seguridad
+
 app.use(cookieParser());
 app.use(cors({
-    origin: ['http://13.60.193.203', 'http://localhost:8000'],  // Reemplaza con la IP o dominio de tu frontend
-    credentials: true,               // Para permitir cookies
-    methods: 'GET,POST,PUT,DELETE',  // Métodos permitidos
-    allowedHeaders: 'Content-Type,Authorization' // Cabeceras permitidas
+    origin: ['http://13.60.193.203', 'http://localhost:8000', 'http://localhost:5173'],  
+    credentials: true,               
+    methods: 'GET,POST,PUT,DELETE',  
+    allowedHeaders: 'Content-Type,Authorization' 
 }));
 app.use(helmet());
 app.use(morgan('tiny'));
 
-// Middlewares para manejo de datos
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Manejo de rutas y errores
 app.use('/pet', petRouter);
 app.use(errorHandler);
 
